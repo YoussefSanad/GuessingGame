d5b1d75e7f01c585d4ea21d2593ea13edc4cc1e5
youssefSanad <yosefsanad11@gmail.com>
2022-02-27 01:13:25 +0200

    creating game & view

---
+++ resources/views/games/game_card.blade.php
@@ -0,0 +1,9 @@
+<div class="game-card center">
+    <div class="game-content">
+        <form action="submit_guess" class="game-form">
+            <input type="number" step="1" validate class="guessing-input">
+
+            <input type="submit" class="submit" value="Guess Number">
+        </form>
+    </div>
+</div>
