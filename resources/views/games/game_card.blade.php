e3bedd2b43e7b92cf76d8a6a04872383a7874a57
youssefSanad <yosefsanad11@gmail.com>
2022-02-27 15:44:06 +0200

    styling the elements

--- resources/views/games/game_card.blade.php
+++ resources/views/games/game_card.blade.php
@@ -1,14 +1,20 @@
-<div class="game-card center">
-    <div class="game-content">
-        <form action="/api/games/{{$activeGame->id}}/users/{{auth()->id()}}/guess" class="game-form"
-              data-user-id="{{auth()->id()}}">
-            <input type="number" step="1" min="1" max="100" validate class="guessing-input">
+<div class="main-card">
 
-            <input type="submit" class="submit" value="Guess Number">
-        </form>
+    <div class="header">
+        <img class="user-image center" src="{{asset('img/unknown-user.png')}}" alt="User Image" height="50px" width="50px">
+        <h1 class="title user-name">{{auth()->user()->name}}</h1>
     </div>
 
-    <div id="result">
+    <form action="/api/games/{{$activeGame->id}}/users/{{auth()->id()}}/guess" class="game-form"
+          data-user-id="{{auth()->id()}}">
+        <input type="number" step="1" min="1" max="100" validate required class="guessing-input center" placeholder="Guess a number..">
 
+        <input type="submit" class="btn btn-primary" value="Guess Number">
+    </form>
+</div>
+
+<div class="main-card">
+    <div id="result" class="correct-result">
+        Something for now
     </div>
 </div>
